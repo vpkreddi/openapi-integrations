a06b3ba5128fee6a490819f3d74b8b87897dd272
venkata <kvpreddii@gmail.com>
2020-12-21 10:08:47 +0530

    swagger added

--- src/main/java/com/demo/openapi/controller/OpenAPIController.java
+++ src/main/java/com/demo/openapi/controller/OpenAPIController.java
@@ -2,19 +2,31 @@ package com.demo.openapi.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.demo.openapi.domain.ValidatePhoneNumberResponseDTO;
 import com.demo.openapi.services.OpenPiService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+
 @RestController
+@Api
 public class OpenAPIController {
 
 	@Autowired
 	private OpenPiService service;
 
-	@PostMapping("/validate/phoneNumbers")
-	public ValidatePhoneNumberResponseDTO validatePhoneNumber(String[] numbers) {
+	@ApiOperation(value = "validate phone number")
+	@PostMapping(value="/validate/phoneNumbers")
+	public ValidatePhoneNumberResponseDTO validatePhoneNumber(
+			@ApiParam(
+					name =  "input",
+				    type = "String",
+				    value = "comma separated strings",
+				    example = "91-1234567890,91-234455") @RequestParam(defaultValue = "91-1234567890,91-234455") String[] numbers) {
 		return service.validate(numbers);
 	}
 }
